c019bf9dd74f9dbdc28736caa208b95aa1a60f8f
theoretical <theoreticalbts@users.noreply.github.com>
2020-06-02 16:14:44 -0400

    Use venv instead of virtualenv

--- ci/install.sh
+++ ci/install.sh
@@ -7,13 +7,13 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
       python3 \
       python3-pip \
       python3-setuptools \
+      python3-venv \
       libsnappy-dev \
       zlib1g-dev \
       libbz2-dev \
       liblz4-dev \
       libzstd-dev \
-      valgrind \
-      virtualenv
+      valgrind
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
    brew install cmake \
       boost \
@@ -30,7 +30,8 @@ KOINOS_VE=$( $(dirname "$0")/find_ve.sh )
 if [ ! -e "$KOINOS_VE" ]
 then
     echo "Creating new virtualenv in $KOINOS_VE"
-    virtualenv -p $(which python3) "$KOINOS_VE"
+    python3 -m venv "$KOINOS_VE"
 fi
 
-"$KOINOS_VE/bin/pip3" install dataclasses-json Jinja2 importlib_resources pluginbase
+"$KOINOS_VE/bin/pip" install wheel
+"$KOINOS_VE/bin/pip" install dataclasses-json Jinja2 importlib_resources pluginbase
