44b984232920bdc8100f5c83e3c939cfbbd2f617
youkaicountry <youkaicountry@users.noreply.github.com>
2020-06-17 16:05:29 -0400

    Change example smart contract to grab argument

--- wasm/hello.c
+++ wasm/hello.c
@@ -1,10 +1,13 @@
-
 #include <stdint.h>
 
 void prints( const char* str );
+uint32_t read_contract_args( char* memory, uint32_t buffer_size );
 
 __attribute__( (visibility("default")) )
 void apply( uint64_t a, uint64_t b, uint64_t c )
 {
-   prints( "Greetings from koinos vm" );
+   char message[32];
+   uint32_t size = read_contract_args(message, 31);
+   message[size] = 0;
+   prints( message );
 }
